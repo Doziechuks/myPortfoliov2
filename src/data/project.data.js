ef11091ef359a52c466cd97ec3a8d83d100285d5
doziechuks <doziechuks1010@gmail.com>
2023-03-31 14:25:17 +0100

    setup data files and assets folder

---
+++ src/data/project.data.js
@@ -0,0 +1,78 @@
+import chat from "../../assets/chat.jpg";
+import shop from "../../assets/shop5.jpg";
+import paco from "../../assets/landingPagepng.png";
+import paco2023 from "../../assets/paco.jpg";
+import movie from "../../assets/movie.jpg";
+import todo from "../../assets/to-do.jpg";
+import quiz from "../../assets/quiz.jpg";
+import jinli from "../../assets/jinli.png";
+import myPortfolio from "../../assets/myportfolio.png";
+
+const projectData = [
+  {
+    title: "chat app",
+    imageUrl: chat,
+    size: "large",
+    id: 1,
+    gitLink: "https://github.com/Doziechuks/chatApp",
+    liveUrl: "https://letstalk-rho.vercel.app/",
+  },
+  {
+    title: "e-commerce app",
+    imageUrl: shop,
+    id: 2,
+    gitLink: "https://github.com/Doziechuks/e-commerce",
+    liveUrl: "https://glamourluxuries.vercel.app/",
+  },
+  {
+    title: "PACO energy",
+    imageUrl: paco,
+    id: 3,
+    gitLink: "https://github.com/Doziechuks/paco-enegy-services",
+    liveUrl: "https://pacoenegyservices.vercel.app/",
+  },
+  {
+    title: "PACO 2023",
+    imageUrl: paco2023,
+    id: 4,
+    gitLink: "https://github.com/Doziechuks/pacowedding",
+    liveUrl: "https://paco2023.vercel.app/",
+  },
+  {
+    title: "movie serach app",
+    imageUrl: movie,
+    id: 5,
+    gitLink: "https://github.com/Doziechuks/movie-search-app",
+    liveUrl: "https://moviesearchapptest.vercel.app/",
+  },
+  {
+    title: "todo app",
+    imageUrl: todo,
+    id: 6,
+    gitLink: "https://github.com/Doziechuks/react-to-do-app",
+    liveUrl: "https://mytodoapp-livid.vercel.app/",
+  },
+  {
+    title: "quiz app",
+    imageUrl: quiz,
+    id: 7,
+    gitLink: "https://github.com/Doziechuks/quizApp",
+    liveUrl: "https://skilltest-eight.vercel.app/",
+  },
+  {
+    title: "jinli spare parts",
+    imageUrl: jinli,
+    id: 8,
+    gitLink: "https://github.com/Doziechuks/Jinli-portfolio",
+    liveUrl: "https://sample-gamma-nine.vercel.app/",
+  },
+  {
+    title: "my portfolio",
+    imageUrl: myPortfolio,
+    id: 9,
+    gitLink: "https://github.com/Doziechuks/obodozie_chuka",
+    liveUrl: "https://glamour-clothing-app.vercel.app/",
+  },
+];
+
+export default projectData;
---
+++ src/data/skills.data.js
@@ -0,0 +1,50 @@
+import html from "../../assets/html.png";
+import css from "../../assets/css.png";
+import js from "../../assets/js.png";
+import react from "../../assets/react.png";
+import redux from "../../assets/redux.png";
+import firebase from "../../assets/firebase.png";
+import tailwind from "../../assets/tailwind.png";
+import materialUI from "../../assets/material UI.png";
+import git from "../../assets/git.png";
+
+const skillsData = [
+  {
+    id: 1,
+    imageUrl: html,
+  },
+  {
+    id: 2,
+    imageUrl: css,
+  },
+  {
+    id: 3,
+    imageUrl: js,
+  },
+  {
+    id: 4,
+    imageUrl: react,
+  },
+  {
+    id: 5,
+    imageUrl: redux,
+  },
+  {
+    id: 6,
+    imageUrl: firebase,
+  },
+  {
+    id: 7,
+    imageUrl: tailwind,
+  },
+  {
+    id: 8,
+    imageUrl: materialUI,
+  },
+  {
+    id: 9,
+    imageUrl: git,
+  },
+];
+
+export default skillsData;
